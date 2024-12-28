7751b18c4ef6abb0d0618a9b0033488849c6c98b
AmeerUlAman <w.ameerulaman@gmail.com>
2024-12-28 23:53:25 +0500

    structure apprx + minor styles fixes

--- src/app/login/login.tsx
+++ src/app/login/login.tsx
@@ -4,7 +4,22 @@ import styles from './login.module.css';
 const Login=()=>{
 return(
     <div className={styles.loginwhole}>
+        <center><h1>Log in</h1></center>
         
+        <form className={styles.ver}>
+           <div className={styles.wholefield}>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" className={styles.field}  required/>
+           </div>
+           <div className={styles.wholefield}>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" className={styles.field}  required/>
+           </div>
+            <button type="submit" className={styles.loginbut}>Log in</button> 
+            
+
+
+        </form>
     </div>
 );
 }
